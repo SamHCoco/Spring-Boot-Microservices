6b9aca036ff199853c1ad3c110b80ac8eecc5ca1
Sam Coco <Samuel.HenriAC@outlook.com>
2020-01-11 22:29:06 +0000

    Wrapper object for Car JSON arrays

---
+++ Car-Rental-Microservice/car-catalogue-service/src/main/java/com/SamHCoco/microservices/car/catalogue/model/Cars.java
@@ -0,0 +1,20 @@
+package com.SamHCoco.microservices.car.catalogue.model;
+
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+}
