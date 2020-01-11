14575a773f9033428dc384eb91230f893a74cdc4
Sam Coco <Samuel.HenriAC@outlook.com>
2020-01-11 22:25:16 +0000

    Main method to run service

---
+++ Car-Rental-Microservice/car-catalogue-service/src/main/java/com/SamHCoco/microservices/car/catalogue/CarCatalogueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.SamHCoco.microservices.car.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCatalogueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCatalogueServiceApplication.class, args);
+	}
+
+}
