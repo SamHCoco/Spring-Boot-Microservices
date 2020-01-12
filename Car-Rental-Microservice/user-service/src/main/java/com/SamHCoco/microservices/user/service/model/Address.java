0eabedd4c7e10b8b930a080166d4a0218172bd36
Sam Coco <Samuel.HenriAC@outlook.com>
2020-01-12 12:56:33 +0000

    Model class for user home address

---
+++ Car-Rental-Microservice/user-service/src/main/java/com/SamHCoco/microservices/user/service/model/Address.java
@@ -0,0 +1,58 @@
+package com.SamHCoco.microservices.user.service.model;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    private String street;
+    private String city;
+    private String postcode;
+    private String location;
+
+    // empty constructor for Hibernate to work with
+    public Address(){
+
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    @Override
+    public String toString(){
+        return "  \nADDRESS: \n" +
+                "  street: " + street + "\n" +
+                "  city: " + city + "\n" +
+                "  Postcode: " + postcode + "\n" +
+                "  location: " + location + "\n";
+    }
+}
